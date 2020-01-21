29cdbe1826059c218678a22beaa52ee4d0a281bf
Kelynn <Kelynn.c19@gmail.com>
2020-01-20 20:15:55 -0800

    commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Kelynn Ma on 1/16/20.
+//  Copyright © 2020 kelynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true);
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calculate tip & total
+        let tipPercentages = [0.15,0.18,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update tip & total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+}
+
